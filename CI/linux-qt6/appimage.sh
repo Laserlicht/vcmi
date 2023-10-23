5cb96d3716ccc88b48954e69b7a943ea58b63ce5
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2023-10-24 01:03:38 +0200

    deb and appimage

---
+++ CI/linux-qt6/appimage.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cat > vcmi.yml << EOF
+app: vcmi
+ 
+ingredients:
+  dist: jammy
+  debs: 
+    - $DEB_PATH/*
+ 
+script:
+  - mv usr/lib/x86_64-linux-gnu/vcmi/* usr/lib/x86_64-linux-gnu/
+  - mv usr/games/* usr/share/vcmi/* -t usr/bin
+  - mv usr/lib/x86_64-linux-gnu/AI usr/bin/AI
+  - sed -i -e 's#/usr#././#g' usr/bin/vcmiclient usr/bin/vcmilauncher usr/bin/vcmiserver usr/bin/vcmieditor
+  - cp usr/share/applications/vcmilauncher.desktop vcmi.desktop
+  - cp usr/share/icons/hicolor/scalable/apps/vcmiclient.svg vcmiclient.svg
+EOF
+
+wget -c $(wget -q https://api.github.com/repos/AppImageCommunity/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
+chmod +x ./pkg2appimage-*.AppImage
+./pkg2appimage-*.AppImage vcmi.yml
