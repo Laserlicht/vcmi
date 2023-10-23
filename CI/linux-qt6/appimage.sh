86a6d7d247f13036f5469cc7e5e746ad672f87f0
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2023-10-24 01:11:07 +0200

    path

--- CI/linux-qt6/appimage.sh
+++ CI/linux-qt6/appimage.sh
@@ -16,6 +16,7 @@ script:
   - cp usr/share/icons/hicolor/scalable/apps/vcmiclient.svg vcmiclient.svg
 EOF
 
+cd $DEB_PATH
 wget -c $(wget -q https://api.github.com/repos/AppImageCommunity/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
 chmod +x ./pkg2appimage-*.AppImage
 ./pkg2appimage-*.AppImage vcmi.yml
