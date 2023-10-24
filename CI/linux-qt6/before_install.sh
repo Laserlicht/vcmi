5b10963382f0b8688288fc9f52777ed550b92225
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2023-10-24 02:13:55 +0200

    lib and appimage

--- CI/linux-qt6/before_install.sh
+++ CI/linux-qt6/before_install.sh
@@ -7,4 +7,5 @@ sudo apt-get install libboost-all-dev \
 libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
 qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools qt6-l10n-tools \
 ninja-build zlib1g-dev libavformat-dev libswscale-dev libtbb-dev libluajit-5.1-dev \
+fuse libfuse2 \
 libminizip-dev libfuzzylite-dev # Optional dependencies
