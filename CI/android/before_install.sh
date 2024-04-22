ed8dedf6ac0bdce90c78cc17d24d0446b671937d
Andrey Filipenkov <decapitator@ukr.net>
2024-04-22 13:33:00 +0300

    fix java

--- CI/android/before_install.sh
+++ CI/android/before_install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "ANDROID_NDK_ROOT=$ANDROID_HOME/ndk/25.2.9519653" >> $GITHUB_ENV
-echo "JAVA_HOME=$JAVA_HOME_11_X64" >> $GITHUB_ENV
+# echo "JAVA_HOME=$JAVA_HOME_11_X64" >> $GITHUB_ENV
+echo "PATH=$JAVA_HOME_11_X64/bin:$PATH" >> $GITHUB_ENV
 
 brew install ninja
 
